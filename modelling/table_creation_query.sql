41587c872e7b5ea1d893d9d54a50cdfe8397742a
Example User <eUser@example.com>
2023-08-17 04:46:56 +0000

    Added Table creation SQL to ingest the required tables

---
+++ modelling/table_creation_query.sql
@@ -0,0 +1,4 @@
+CREATE TABLE attendance_data_final_2022 AS SELECT * FROM read_csv_auto('data/2022-attendance-data-final.csv');
+
+CREATE TABLE multi_age_composite_classes_final_2022 AS SELECT * FROM read_csv_auto('data/2022-multi-age_composite-classes-final.csv');
+
